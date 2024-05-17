91486d39d058ec62bb3ba14734f67e8370bb6c42
noaru35 <vinicius.s.bastos55@gmail.com>
2024-05-16 22:32:28 -0300

    adicionei o arquivo AuthController.php

--- src/main.php
+++ src/main.php
@@ -5,4 +5,7 @@ include_once (__dir__ . '/../vendor/autoload.php');
 
 Use App\model\Todo;
 $todo1 = new Todo(1, 'Aprendendo PHP');
+$todo2 = new Todo(2, 'Testando o comando');
 print_r($todo1->title);
+echo "<br>";
+print_r($todo2->title);
